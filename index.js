377c2b667234f8fd33984425ccba0b005abbc0ba
unknown <bartbragiel@gmail.com>
2019-04-06 14:03:19 -0500

    second commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.info('console.info');
+
+console.error('console.this is an error');
+
+
+console.log($);
+$(document ).ready(function(){
+    //Hander for .ready() called.
+    console.log('i am ready')
+});
